63271207749885e4a0859c9e34539c4f61636a59
Ruslan Kuzma <kuzrus31@gmail.com>
2021-08-13 15:59:16 +0300

    вставил css стили

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import './sass/main.scss';
+import './sass/main.scss'; 
 import _ from 'lodash'
-import countriesCharacteristics from './tampletes/countries.hbs';
-import countriesName from './tampletes/countriesName.hbs';
-import fetchCountries from './fechCountries.js'
+import countriesCharacteristics from './tampletes/countries.hbs';//Импорт шаблонизатора характеристик стран
+import countriesName from './tampletes/countriesName.hbs'; //Импорт шаблонизатора  страны
+import fetchCountries from './fechCountries.js'//Импорт  HTTP запроса
 import './sass/main.scss';
-import error from './tampletes/pnitify.js'
+import error from './tampletes/pnitify.js'// Импорт плагина pnitify для оповещений
 
 
 import { options } from 'colorette';
@@ -23,14 +23,17 @@ const refs = {
 
            
 refs.inputContries.addEventListener('input',  _.debounce(renderCounriesCard, 1000))
-       
+       //Создаем функцию для вывода информации о стране
 function renderCounriesCard(e) {
+
     e.preventDefault()
+    //Создаем переменную для вывода информации елементов в окно браузера
     const filter = e.target.value.trim();
 
     clearContainerCountries();
-
+//Выводим значение в зависимости от полученого к-ва стран
     fetchCountries(filter).then(data => {
+
         const markup = listCountriesName(data);
 
         const country = oneCountries(data);
@@ -45,7 +48,7 @@ function renderCounriesCard(e) {
                     
                  delay: 2000,    
             })
-        //  refs.textMessage.insertAdjacentHTML('beforeend', message); 
+        
         } else if  (data.length >= 2 && data.length <= 10){
             
             insertListItem(country);
@@ -60,6 +63,7 @@ function renderCounriesCard(e) {
 
 }
 
+//Функция которая вставляет указанные  шаблони в DOM дерево
 function insertListItem(items) {
   refs.cardContainer.insertAdjacentHTML('beforeend', items);
 }
